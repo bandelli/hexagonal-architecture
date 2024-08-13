4df456580aafd29cbf2c4772c979967bdfd4eb1a
Mari Martin <bandelli@gmail.com>
2024-08-12 21:12:58 -0300

    ADD hexagonal

--- src/main/java/br/com/fullcycle/hexagonal/controllers/CustomerController.java
+++ src/main/java/br/com/fullcycle/hexagonal/controllers/CustomerController.java
@@ -1,7 +1,9 @@
 package br.com.fullcycle.hexagonal.controllers;
 
+import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
+import br.com.fullcycle.hexagonal.application.usescases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.application.usescases.GetCustomerByIdUserCase;
 import br.com.fullcycle.hexagonal.dtos.CustomerDTO;
-import br.com.fullcycle.hexagonal.models.Customer;
 import br.com.fullcycle.hexagonal.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+// Adapter
 @RestController
 @RequestMapping(value = "customers")
 public class CustomerController {
@@ -16,32 +19,24 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+
     @PostMapping
     public ResponseEntity<?> create(@RequestBody CustomerDTO dto) {
-        if (customerService.findByCpf(dto.getCpf()).isPresent()) {
-            return ResponseEntity.unprocessableEntity().body("Customer already exists");
+        try {
+            final var useCase = new CreateCustomerUseCase(customerService);
+            final var output = useCase.execute(new CreateCustomerUseCase.Input(dto.getCpf(), dto.getEmail(), dto.getName()));
+            return ResponseEntity.created(URI.create("/customers/" + output.id())).body(output);
+        } catch (ValidationException ex) {
+            return ResponseEntity.unprocessableEntity().body(ex.getMessage());
         }
-        if (customerService.findByEmail(dto.getEmail()).isPresent()) {
-            return ResponseEntity.unprocessableEntity().body("Customer already exists");
-        }
-
-        var customer = new Customer();
-        customer.setName(dto.getName());
-        customer.setCpf(dto.getCpf());
-        customer.setEmail(dto.getEmail());
-
-        customer = customerService.save(customer);
-
-        return ResponseEntity.created(URI.create("/customers/" + customer.getId())).body(customer);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> get(@PathVariable Long id) {
-        var customer = customerService.findById(id);
-        if (customer.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
 
-        return ResponseEntity.ok(customer.get());
+        final var useCase = new GetCustomerByIdUserCase(customerService);
+        return useCase.execute(new GetCustomerByIdUserCase.Input(id))
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
     }
 }
---
+++ src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
@@ -0,0 +1,25 @@
+package br.com.fullcycle.hexagonal.graphql;
+
+import br.com.fullcycle.hexagonal.application.usescases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.dtos.CustomerDTO;
+import br.com.fullcycle.hexagonal.services.CustomerService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+// Adapter
+@Controller
+public class CustomerResolver {
+    private final CustomerService customerService;
+
+
+    public CustomerResolver(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @MutationMapping
+    public CreateCustomerUseCase.Output createCustomer(@Argument CustomerDTO input) {
+        final var useCase = new CreateCustomerUseCase(customerService);
+        return useCase.execute(new CreateCustomerUseCase.Input(input.getCpf(), input.getEmail(), input.getName()));
+    }
+}
