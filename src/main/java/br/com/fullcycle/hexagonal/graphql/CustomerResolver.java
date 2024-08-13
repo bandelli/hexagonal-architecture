2619ebb8c7de9dac5f5cb09542df8b88d998e5c0
Mari Martin <bandelli@gmail.com>
2024-08-12 22:23:29 -0300

    Fix graphql

--- src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
+++ src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
@@ -1,10 +1,12 @@
 package br.com.fullcycle.hexagonal.graphql;
 
 import br.com.fullcycle.hexagonal.application.usescases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.application.usescases.GetCustomerByIdUserCase;
 import br.com.fullcycle.hexagonal.dtos.CustomerDTO;
 import br.com.fullcycle.hexagonal.services.CustomerService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 // Adapter
@@ -21,5 +23,11 @@ public class CustomerResolver {
     public CreateCustomerUseCase.Output createCustomer(@Argument CustomerDTO input) {
         final var useCase = new CreateCustomerUseCase(customerService);
         return useCase.execute(new CreateCustomerUseCase.Input(input.getCpf(), input.getEmail(), input.getName()));
+    }
+
+    @QueryMapping
+    public GetCustomerByIdUserCase.Output customerOfId(@Argument Long id) {
+        final var useCase = new GetCustomerByIdUserCase(customerService);
+        return useCase.execute(new GetCustomerByIdUserCase.Input(id)).orElse(null);
     }
 }
