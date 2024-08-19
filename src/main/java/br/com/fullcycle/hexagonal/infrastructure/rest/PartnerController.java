e428bb30726958ed56408e22b8f40732e6bcd060
Mari Martin <bandelli@gmail.com>
2024-08-18 22:09:46 -0300

    Refac

---
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/rest/PartnerController.java
@@ -0,0 +1,46 @@
+package br.com.fullcycle.hexagonal.infrastructure.rest;
+
+import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
+import br.com.fullcycle.hexagonal.application.usescases.CreatePartnerUseCase;
+import br.com.fullcycle.hexagonal.application.usescases.GetPartnerByIdUseCase;
+import br.com.fullcycle.hexagonal.infrastructure.dtos.NewPartnerDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Objects;
+
+@RestController
+@RequestMapping(value = "partners")
+public class PartnerController {
+
+    private final CreatePartnerUseCase createPartnerUseCase;
+    private final GetPartnerByIdUseCase getPartnerByIdUseCase;
+
+    public PartnerController(CreatePartnerUseCase createPartnerUseCase, GetPartnerByIdUseCase getPartnerByIdUseCase) {
+        this.createPartnerUseCase = Objects.requireNonNull(createPartnerUseCase);
+        this.getPartnerByIdUseCase = Objects.requireNonNull(getPartnerByIdUseCase);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody NewPartnerDTO dto) {
+        try {
+
+            final var output = createPartnerUseCase.execute(new CreatePartnerUseCase.Input(dto.cnpj(), dto.name(), dto.email()));
+
+            return ResponseEntity.created(URI.create("/partners/" + output.id())).body(output);
+        } catch (ValidationException ex) {
+            return ResponseEntity.unprocessableEntity().body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable Long id) {
+
+
+        return getPartnerByIdUseCase.execute(new GetPartnerByIdUseCase.Input(id))
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
+    }
+
+}
--- src/test/java/br/com/fullcycle/hexagonal/application/usescases/CreateEventUseCaseIT.java
+++ src/test/java/br/com/fullcycle/hexagonal/application/usescases/CreateEventUseCaseIT.java
@@ -1,9 +1,12 @@
 package br.com.fullcycle.hexagonal.application.usescases;
 
 
+import br.com.fullcycle.hexagonal.application.IntegrationTest;
 import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
 import br.com.fullcycle.hexagonal.infrastructure.models.Event;
 import br.com.fullcycle.hexagonal.infrastructure.models.Partner;
+import br.com.fullcycle.hexagonal.infrastructure.repositories.EventRepository;
+import br.com.fullcycle.hexagonal.infrastructure.repositories.PartnerRepository;
 import br.com.fullcycle.hexagonal.infrastructure.services.EventService;
 import br.com.fullcycle.hexagonal.infrastructure.services.PartnerService;
 import io.hypersistence.tsid.TSID;
@@ -12,6 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
@@ -21,7 +25,22 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
-class CreateEventUseCaseTest {
+class CreateEventUseCaseIT extends IntegrationTest {
+
+    @Autowired
+    private CreateEventUseCase useCase;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @BeforeEach
+    void tearDown() {
+        partnerRepository.deleteAll();
+        eventRepository.deleteAll();
+    }
 
     @Mock
     private EventService eventService;
@@ -37,14 +56,14 @@ class CreateEventUseCaseTest {
     @DisplayName("Deve criar um evento")
     void deveCriarUmEvento() {
         //given
+        final var partner = createPartner("41536538000100", "john.doe@gmail.com", "John Doe");
 
         final var expectedDate = "2021-01-01";
         final var expectedName = "Disney on Ice";
         final var expectedTotalSpots = 10;
-        final var expectedTPartnerId = TSID.fast().toLong();
+        final var expectedTPartnerId = partner.getId();
 
         final var createIpunt =
-
                 new CreateEventUseCase.Input(expectedDate, expectedName, expectedTPartnerId, expectedTotalSpots);
 
         //when
@@ -86,17 +105,20 @@ class CreateEventUseCaseTest {
         final var createInput = new CreateEventUseCase.Input(expectedDate, expectedName, expectedTPartnerId, expectedTotalSpots);
 
         //when
-        final var eventService = mock(EventService.class);
-        final var partnerService = mock(PartnerService.class);
 
-        when(partnerService.findById(eq(expectedTPartnerId)))
-                .thenReturn(Optional.empty());
-
-        final var useCase = new CreateEventUseCase(eventService, partnerService);
         final var actualExeption = Assertions.assertThrows(ValidationException.class, () ->
                 useCase.execute(createInput));
 
         //then
         Assertions.assertEquals(expectedError, actualExeption.getMessage());
+    }
+
+    private Partner createPartner(final String cnpj, final String email, final String name) {
+        Partner aPartner = new Partner();
+        aPartner.setCnpj(cnpj);
+        aPartner.setEmail(email);
+        aPartner.setName(name);
+        return partnerRepository.save(aPartner);
+
     }
 }
